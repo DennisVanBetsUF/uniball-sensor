9d53e0a4ed839ab316e35d0f1cf540ca30a58cad
Dennis <dennis.vanbets@unifly.aero>
2020-01-19 23:45:02 +0100

    kafka configuration outside localhost with dns

--- src/app.js
+++ src/app.js
@@ -1,26 +1,12 @@
 const kafka = require('kafka-node');
 var gpio = require('rpi-gpio');
 
-const client = new kafka.KafkaClient({kafkaHost: '192.168.1.153:9094'});
+const client = new kafka.KafkaClient({kafkaHost: 'http://games.unifly.aero:29092'});
 
 var Producer = kafka.Producer,
     producer = new Producer(client);
 
-console.log('starting...');
-producer.on('ready', function () {
-    console.log('Data broker ready');
-});
-
-gpio.on('change', function (channel, value) {
-    if (channel == 13 && value == false) {
-      producer.send([{
-          topic: 'score',
-          messages: [2], //red
-          timestamp: Date.now()
-       }],(error) => console.log('score red sent', error));
-    }
-    if (channel == 37 && value == false) {
-        producer.send([{
+producer.send([{
             topic: 'score',
             messages: [1], //blue
 	        timestamp: Date.now()
